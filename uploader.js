cadac4289d7bfac2628edca6c1928d0dd5c4df0f
Gedas11 <97019328+Gedas11@users.noreply.github.com>
2022-05-10 11:06:45 +0300

    nuotrauku-atvaizdavimas

--- uploader.js
+++ uploader.js
@@ -1,43 +1,73 @@
-import database from './helpers/config.js'
-import {port} from './helpers/config.js'
-import mysql from 'mysql2/promise'
-import express from 'express'
-import {create} from 'express-handlebars'
-import multer from 'multer'
 
-const app = express()
-const handlebars = create()
-//const multer = multer()
+import { port } from "./helpers/config.js";
+import db from "./helpers/db.js";
+
+import express from "express";
+import { create } from "express-handlebars";
+import multer from "multer";
+import {rm} from  'fs/promises'
+
+const app = express();
+const handlebars = create();
+
 
 const diskStorage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, './uploads')
-    }
-})
+  destination: (req, file, callback) => {
+    callback(null, "./uploads");
+  },
+  filename: (req, file, callback) => {
+    const ext = file.originalname.split(".");
+
+    callback(null, Date.now() + "." + ext[1]);
+  },
+});
 
 const upload = multer({
-    storage: diskStorage
-})
+  storage: diskStorage,
+  fileFilter: (req, file, callback) => {
+      //atliekamas failu trikinimas
+      
+      if(file.mimetype === 'image/jpeg' || 
+      file.mimetype === 'image/png' || 
+      file.mimetype === 'image/gif'
+      ) 
+      callback(null, true)
+      else
+      callback(null, false)
+  }
+});
 
-app.engine('handlebars', handlebars.engine)
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
-const connection = await mysql.createConnection({
-    host: database.database_host,
-    user: database.user,
-    database: database.database
-})
+app.use('/uploads', express.static('uploads'))
 
-const table = 'pictures'
 
-app.get('/', (req, res) => {
-    res.render('form')
-})
 
-app.post('/upload_file', upload.single('photo'), (req, res) => {
-    console.log(req.file)
-    res.send('Failas gautas')
-})
+app.get("/", async(req, res) => {
+    const photos = await db.query('SELECT * FROM pictures')
+    
+  res.render("form", {photos: photos[0]});
+});
 
-app.listen(port)
+app.post("/upload_file", upload.single("photo"), async(req, res) => {
+  if(req.file === undefined){
+      res.send('netinkamas failas')
+  } else {
+      
+    
+    const insert = await db.query('INSERT INTO pictures (picture) VALUES (?)', req.file.filename)
+    
+    
+
+    if(insert[0].affectedRows === 1){
+        res.send("Failas gautas");
+        }    else {
+            await rm('./uploads/' + req.file.filename)
+        res.send('nepavyko ikelti failo')
+        }
+  }
+});
+
+app.listen(port);
